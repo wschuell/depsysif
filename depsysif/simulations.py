fe8977b16815d83ac25ed4db29a6da351e2c39d4
William Schueller <william.schueller@gmail.com>
2020-08-28 08:47:39 +0200

    simulations file

---
+++ depsysif/simulations.py
@@ -0,0 +1,12 @@
+class Simulation(object):
+	'''
+	Simulation objects simulate cascades of failures in projects through the dependency hierarchy
+	'''
+	def __init__(self):
+		pass
+
+	def run(self):
+		pass
+
+	def set_network(self):
+		pass
